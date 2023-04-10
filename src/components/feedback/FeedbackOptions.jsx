ab34c9e881c22f46018a31087b92d89410fd259b
Sforka <warlock20115@gmail.com>
2023-04-10 15:45:43 +0300

    1
    1

--- src/components/feedback/FeedbackOptions.jsx
+++ src/components/feedback/FeedbackOptions.jsx
@@ -1,4 +1,3 @@
-
 import PropTypes from 'prop-types';
 
 export const FeedbackOptions = ({ options, dispatch }) => {
@@ -19,7 +18,6 @@ export const FeedbackOptions = ({ options, dispatch }) => {
 };
 
 FeedbackOptions.propTypes = {
-  options: PropTypes.array.isRequired,
   dispatch: PropTypes.func.isRequired,
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
-  
